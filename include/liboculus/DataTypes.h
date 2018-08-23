143844d0465254cfef3be7eb1ec8c5f39d9bdfb9
Aaron Marburg <aaron@apl.washington.edu>
2018-08-22 22:07:35 -0700

    Added DataTypes.h

---
+++ include/liboculus/DataTypes.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Oculus/Oculus.h"
+
+namespace liboculus {
+
+  inline size_t DataSize( DataSizeType d ) {
+    switch(d) {
+      case dataSize8Bit:
+        return 1;
+      case dataSize16Bit:
+        return 2;
+      case dataSize24Bit:
+        return 3;
+      case dataSize32Bit:
+        return 4;
+    }
+
+    return 0;
+  }
+
+
+}
