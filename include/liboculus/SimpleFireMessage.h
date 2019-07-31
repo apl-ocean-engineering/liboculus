c1d9522f23130280eca7edd089f94628e22b049e
Tanner Poling <tannerpoling@gmail.com>
2019-07-31 13:53:54 -0700

    added accessor methods to SimpleFireMessage

--- include/liboculus/SimpleFireMessage.h
+++ include/liboculus/SimpleFireMessage.h
@@ -14,6 +14,14 @@ public:
 
   void serialize( boost::asio::streambuf &buffer );
 
+  void setGamma(uint8_t input);
+
+  void setPingRate(uint8_t input);
+
+  void setGainPercent(uint8_t input);
+
+  void setRange(uint8_t input);
+
 private:
 
   OculusSimpleFireMessage _sfm;
