0653fdf1a27a3a6f5538bff4088146ec050e2488
Aaron Marburg <amarburg@uw.edu>
2020-04-10 15:06:04 -0700

    Re-instituted logging in main.cpp

--- test/unit/main.cpp
+++ test/unit/main.cpp
@@ -1,12 +1,14 @@
-
 #include <gtest/gtest.h>
+#include <g3log/g3log.hpp>
+#include <g3log/logworker.hpp>
 
 // Run all the declared gtests
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
-  //auto worker = g3::LogWorker::createLogWorker();
-  //libg3logger::G3Logger logWorker( argv[0] );
-  //logWorker.setLevel( DEBUG );
+  
+  auto worker = g3::LogWorker::createLogWorker();
+  worker->addDefaultLogger(argv[0], ".");
+  g3::initializeLogging(worker.get());
 
   return RUN_ALL_TESTS();
 }
