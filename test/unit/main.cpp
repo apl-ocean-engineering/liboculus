8e22cfdaadaa98aedabd7b2ac27c82d7d485cad8
Aaron Marburg <amarburg@uw.edu>
2020-04-10 14:56:19 -0700

    Include unit testing in Catkin builds.

--- test/unit/main.cpp
+++ test/unit/main.cpp
@@ -1,11 +1,11 @@
 
 #include <gtest/gtest.h>
-#include <libg3logger/g3logger.h>
 
 // Run all the declared gtests
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
-  libg3logger::G3Logger logWorker( argv[0] );
+  //auto worker = g3::LogWorker::createLogWorker();
+  //libg3logger::G3Logger logWorker( argv[0] );
   //logWorker.setLevel( DEBUG );
 
   return RUN_ALL_TESTS();
