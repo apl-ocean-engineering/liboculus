53c08f2700d1829a7dbeaef79ada2e6293ae7801
Aaron Marburg <amarburg@uw.edu>
2020-06-18 21:04:20 -0700

    Stripped out gpmf stuff.

--- include/liboculus/SonarPlayer.h
+++ include/liboculus/SonarPlayer.h
@@ -31,34 +31,34 @@
 
 #include <fstream>
 
-#ifdef WITH_GPMF
-  #include "gpmf-parser/GPMF_parser.h"
-#endif
-
 #include "liboculus/SimplePingResult.h"
 
 namespace liboculus {
 
 class SonarPlayerBase {
 public:
-  SonarPlayerBase();
-  virtual ~SonarPlayerBase();
+  SonarPlayerBase()           {;}
+  virtual ~SonarPlayerBase()  {;}
 
   virtual bool open(const std::string &filename);
-  virtual bool isOpen() const { return _input.is_open(); }
+  virtual bool isOpen() const
+    { return _input.is_open(); }
 
-  virtual bool eof() const { return _input.eof(); }
-  virtual void rewind() { _input.seekg(0); }
+  virtual bool eof() const
+    { return _input.eof(); }
+
+  virtual void rewind()
+    { _input.seekg(0); }
 
   virtual bool nextPing( SimplePingResult &ping ) = 0;
 
   static std::shared_ptr<SonarPlayerBase> OpenFile(const std::string &filename);
 
-#ifdef WITH_GPMF
-  virtual bool setStream(GPMF_stream *stream) { return false; }
-
-  static std::shared_ptr<SonarPlayerBase> createGPMFSonarPlayer();
-#endif
+// #ifdef WITH_GPMF
+//   virtual bool setStream(GPMF_stream *stream) { return false; }
+//
+//   static std::shared_ptr<SonarPlayerBase> createGPMFSonarPlayer();
+// #endif
 
 protected:
   std::ifstream _input;
@@ -78,48 +78,5 @@ public:
 private:
 };
 
-///
-///
-///
-class OculusSonarPlayer : public SonarPlayerBase {
-public:
-  OculusSonarPlayer();
-  virtual ~OculusSonarPlayer();
-
-  // char *nextPacket();
-  virtual bool nextPing( SimplePingResult &ping );
-
-private:
-};
-
-#ifdef WITH_GPMF
-///
-///
-///
-class GPMFSonarPlayer : public SonarPlayerBase {
-public:
-  GPMFSonarPlayer();
-  virtual ~GPMFSonarPlayer();
-
-  virtual bool open(const std::string &filename);
-  virtual bool setStream(GPMF_stream *stream);
-  virtual bool isOpen() const { return _valid; }
-
-  void close();
-
-  virtual bool eof();
-  virtual void rewind();
-
-  // char *nextPacket();
-  virtual bool nextPing( SimplePingResult &ping );
-
-  void dumpGPMF(void);
-
-private:
-  GPMF_stream _stream;
-  bool _valid;
-  std::string _buffer;
-};
-#endif
 
 } // namespace liboculus
--- lib/SonarPlayer.cpp
+++ lib/SonarPlayer.cpp
@@ -42,11 +42,6 @@ namespace liboculus {
 
 using namespace std;
 
-#ifdef WITH_GPMF
-shared_ptr<SonarPlayerBase> SonarPlayerBase::createGPMFSonarPlayer() {
-  return shared_ptr<SonarPlayerBase>(new GPMFSonarPlayer());
-}
-#endif
 
 /// Static function which automatically detects file type
 shared_ptr<SonarPlayerBase> SonarPlayerBase::OpenFile(const string &filename) {
@@ -63,17 +58,10 @@ shared_ptr<SonarPlayerBase> SonarPlayerBase::OpenFile(const string &filename) {
     f.get(d);
 
     if (d == 0x45) {
-#ifdef WITH_GPMF
-      LOG(INFO) << "I think this is an GPMF file.";
-      return shared_ptr<SonarPlayerBase>(new GPMFSonarPlayer());
-#else
-      LOG(WARNING) << "I think this is a GPMF file, but GPMF support is not compiled in.";
+      LOG(INFO) << "I think this is an GPMF file, unfortunately I cannot parse GPMF";
       return nullptr;
-#endif
     }
 
-    LOG(INFO) << "I think this is an Oculus client file.";
-    return shared_ptr<SonarPlayerBase>(new OculusSonarPlayer());
   } else if (c == 0x53) {
     LOG(INFO) << "I think this is an raw sonar data.";
     return shared_ptr<SonarPlayerBase>(new RawSonarPlayer());
@@ -83,10 +71,6 @@ shared_ptr<SonarPlayerBase> SonarPlayerBase::OpenFile(const string &filename) {
 }
 
 //--- SonarPlayerBase
-
-SonarPlayerBase::SonarPlayerBase() { ; }
-
-SonarPlayerBase::~SonarPlayerBase() { ; }
 
 bool SonarPlayerBase::open(const std::string &filename) {
   _input.open(filename, ios_base::binary | ios_base::in);
@@ -144,152 +128,5 @@ bool RawSonarPlayer::nextPing( SimplePingResult &ping ) {
 
 }
 
-//--- OculusSonarPlayer --
-
-OculusSonarPlayer::OculusSonarPlayer() : SonarPlayerBase() { ; }
-
-OculusSonarPlayer::~OculusSonarPlayer() { ; }
-
-bool OculusSonarPlayer::nextPing( SimplePingResult &ping) {
-  //
-  // Ended up not being able to implement.  Oculus client records
-  // messagePingResult, which we don't have the format for ...
-  //
-
-  return false;
-}
-
-#ifdef WITH_GPMF
-
-//--- GPMFSonarPlayer --
-
-GPMFSonarPlayer::GPMFSonarPlayer()
-    : SonarPlayerBase(), _stream(), _valid(false), _buffer() {
-  ;
-}
-
-GPMFSonarPlayer::~GPMFSonarPlayer() { ; }
-
-bool GPMFSonarPlayer::open(const std::string &filename) {
-  {
-    bool retval = SonarPlayerBase::open(filename);
-    if (!retval)
-      return retval;
-  }
-
-  _input.seekg(0, std::ios::end);
-  const size_t sz = _input.tellg();
-  _buffer.resize(sz, '\0');
-  _input.seekg(0, std::ios::beg);
-
-  _input.read(&_buffer[0], sz);
-
-  // _buffer.assign((std::istreambuf_iterator<char>(_input)),
-  //             std::istreambuf_iterator<char>());
-
-  LOG(DEBUG) << "Loading " << _buffer.size() << " bytes";
-
-  GPMF_Init(&_stream, (unsigned int *)_buffer.c_str(), (_buffer.size()));
-
-  // {
-  //   auto retval = GPMF_Validate(&_stream, GPMF_RECURSE_LEVELS);
-  //   if( retval != GPMF_OK ) {
-  //     LOG(WARNING) << "GPMF structure is not valid; err = " << retval;
-  //     return false;
-  //   }
-  // }
-
-  {
-    auto retval =
-        GPMF_FindNext(&_stream, STR2FOURCC("OCUS"), GPMF_RECURSE_LEVELS);
-
-    if (retval != GPMF_OK) {
-      LOG(INFO) << "Unable to find Oculus sonar data in GPMF file (err "
-                << retval << ")";
-      return false;
-    }
-  }
-
-  return true;
-}
-
-bool GPMFSonarPlayer::setStream(GPMF_stream *stream) {
-  _stream = *stream;
-  // GPMF_Init(&_stream, (unsigned int *)_buffer.c_str(), (_buffer.size()));
-  //
-  // {
-  //   auto retval =
-  //       GPMF_FindNext(&_stream, STR2FOURCC("OCUS"), GPMF_RECURSE_LEVELS);
-  //
-  //   if (retval != GPMF_OK) {
-  //     LOG(INFO) << "Unable to find Oculus sonar data in GPMF file (err "
-  //               << retval << ")";
-  //     return false;
-  //   }
-  // }
-  return true;
-}
-
-bool GPMFSonarPlayer::eof() {
-  // How to handle this?
-  auto retval = GPMF_Validate(&_stream, GPMF_RECURSE_LEVELS);
-  return retval == GPMF_ERROR_BUFFER_END;
-}
-
-void GPMFSonarPlayer::rewind() {
-  if (_valid) {
-    GPMF_ResetState(&_stream);
-  }
-}
-
-void GPMFSonarPlayer::close() { _valid = false; }
-
-void GPMFSonarPlayer::dumpGPMF() {
-  auto key = GPMF_Key(&_stream);
-  LOG(INFO) << "Current key \"" << char((key >> 0) & 0xFF)
-            << char((key >> 8) & 0xFF) << char((key >> 16) & 0xFF)
-            << char((key >> 24) & 0xFF) << "\" (" << std::hex << key << ")";
-  LOG(INFO) << "Current type " << GPMF_Type(&_stream);
-  LOG(INFO) << "Current device ID " << std::hex << GPMF_DeviceID(&_stream);
-
-  char deviceName[80];
-  GPMF_DeviceName(&_stream, deviceName, 79);
-  LOG(INFO) << "Current device name " << deviceName;
-
-  LOG(INFO) << "Current struct size " << GPMF_StructSize(&_stream);
-  LOG(INFO) << "Current repeat size " << GPMF_Repeat(&_stream);
-  LOG(INFO) << "Current payload sample count "
-            << GPMF_PayloadSampleCount(&_stream);
-  LOG(INFO) << "Current elements in struct " << GPMF_ElementsInStruct(&_stream);
-  LOG(INFO) << "Current raw data size " << GPMF_RawDataSize(&_stream);
-}
-
-bool GPMFSonarPlayer::nextPing( SimplePingResult &ping ) {
-  //
-  // Ended up not needing to implement.  Oculus client records
-  // messagePingResult, which we don't have the format for ...
-  auto key = GPMF_Key(&_stream);
-  if (key != STR2FOURCC("OCUS"))
-    return false;
-
-  MessageHeader header( (char *)GPMF_RawData(&_stream), GPMF_RawDataSize(&_stream) );
-  // char *data = (char *)GPMF_RawData(&_stream);
-  // CHECK(data != nullptr);
-
-  if (!header.valid()) {
-    LOG(INFO) << "Invalid header";
-    return false;
-  }
-
-  auto retval = GPMF_FindNext(&_stream, STR2FOURCC("OCUS"), GPMF_RECURSE_LEVELS);
-  if (retval != GPMF_OK) {
-    _valid = false;
-  }
-
-  ping = SimplePingResult( header );
-  return true;
-}
-
-#endif
 
 } // namespace liboculus
