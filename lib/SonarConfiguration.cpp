211f1e1539c11ecc60d6eefb590785bac78f8487
Laura Lindzey <lindzey@uw.edu>
2021-07-12 22:54:36 -0700

    SonarConfiguration: Don't indent in namespace

--- lib/SonarConfiguration.cpp
+++ lib/SonarConfiguration.cpp
@@ -34,123 +34,123 @@
 
 namespace liboculus {
 
-  SonarConfiguration::SonarConfiguration()
-    : _postponeCallback( false ),
-      _callback()
-  {
-    memset( &_sfm, 0, sizeof(OculusSimpleFireMessage));
+SonarConfiguration::SonarConfiguration()
+  : _postponeCallback( false ),
+    _callback()
+{
+  memset( &_sfm, 0, sizeof(OculusSimpleFireMessage));
 
-    _sfm.head.oculusId    = 0x4f53;
-    _sfm.head.msgId       = messageSimpleFire;
-    _sfm.head.msgVersion  = 0;
-    _sfm.head.srcDeviceId = 0;
-    _sfm.head.dstDeviceId = 7892;
-    _sfm.head.payloadSize = sizeof(OculusSimpleFireMessage) - sizeof(OculusMessageHeader);
+  _sfm.head.oculusId    = 0x4f53;
+  _sfm.head.msgId       = messageSimpleFire;
+  _sfm.head.msgVersion  = 0;
+  _sfm.head.srcDeviceId = 0;
+  _sfm.head.dstDeviceId = 7892;
+  _sfm.head.payloadSize = sizeof(OculusSimpleFireMessage) - sizeof(OculusMessageHeader);
 
-    // OculusMessageHeader head;     // The standard message header
-    //
-    // uint8_t masterMode;
-    // * mode 0 is flexi mode, needs full fire message (not available for third party developers)
-    // * mode 1 - Low Frequency Mode (wide aperture, navigation)
-    // * mode 2 - High Frequency Mode (narrow aperture, target identification)
-    //
-    // PingRateType pingRate;        // Sets the maximum ping rate.
-    // uint8_t networkSpeed;         // Used to reduce the network comms speed (useful for high latency shared links)
-    // uint8_t gammaCorrection;      // 0 and 0xff = gamma correction = 1.0
-    //                               // Set to 127 for gamma correction = 0.5
+  // OculusMessageHeader head;     // The standard message header
+  //
+  // uint8_t masterMode;
+  // * mode 0 is flexi mode, needs full fire message (not available for third party developers)
+  // * mode 1 - Low Frequency Mode (wide aperture, navigation)
+  // * mode 2 - High Frequency Mode (narrow aperture, target identification)
+  //
+  // PingRateType pingRate;        // Sets the maximum ping rate.
+  // uint8_t networkSpeed;         // Used to reduce the network comms speed (useful for high latency shared links)
+  // uint8_t gammaCorrection;      // 0 and 0xff = gamma correction = 1.0
+  //                               // Set to 127 for gamma correction = 0.5
 
-    // double range;                 // The range demand in percent or m depending on flags
-    // double gainPercent;           // The gain demand
-    // double speedOfSound;          // ms-1, if set to zero then internal calc will apply using salinity
-    // double salinity;              // ppt, set to zero if we are in fresh water
+  // double range;                 // The range demand in percent or m depending on flags
+  // double gainPercent;           // The gain demand
+  // double speedOfSound;          // ms-1, if set to zero then internal calc will apply using salinity
+  // double salinity;              // ppt, set to zero if we are in fresh water
 
-    _sfm.masterMode      = OCULUS_HIGH_FREQ;
+  _sfm.masterMode      = OCULUS_HIGH_FREQ;
 
-    _sfm.networkSpeed = 0xff;
+  _sfm.networkSpeed = 0xff;
 
-    // Initial values
-    _sfm.gammaCorrection = 127;  //gamma;
-    _sfm.pingRate        = pingRateNormal;
-    _sfm.range           = 2;  // Meters
-    _sfm.gainPercent     = 50;  // gain;
+  // Initial values
+  _sfm.gammaCorrection = 127;  //gamma;
+  _sfm.pingRate        = pingRateNormal;
+  _sfm.range           = 2;  // Meters
+  _sfm.gainPercent     = 50;  // gain;
 
-    // uint8_t flags;
-    // bit 0: 0 = interpret range as percent, 1 = interpret range as meters
-    // bit 1: 0 = 8 bit data, 1 = 16 bit data
-    // bit 2: 0 = wont send gain, 1 = send gain
-    // bit 3: 0 = send full return message, 1 = send simple return message
-    // bit 4: "gain assistance"?
+  // uint8_t flags;
+  // bit 0: 0 = interpret range as percent, 1 = interpret range as meters
+  // bit 1: 0 = 8 bit data, 1 = 16 bit data
+  // bit 2: 0 = wont send gain, 1 = send gain
+  // bit 3: 0 = send full return message, 1 = send simple return message
+  // bit 4: "gain assistance"?
 
-    _sfm.flags = 0x19;  // 0x19 = 0b11001. Send simple return msg; range in meters
+  _sfm.flags = 0x19;  // 0x19 = 0b11001. Send simple return msg; range in meters
 
-    _sfm.speedOfSound    = 0.0;  // m/s  0 for automatic calculation speedOfSound;
-    _sfm.salinity        = 0.0;  // ppt; freshwater salinity;
+  _sfm.speedOfSound    = 0.0;  // m/s  0 for automatic calculation speedOfSound;
+  _sfm.salinity        = 0.0;  // ppt; freshwater salinity;
+}
+
+void SonarConfiguration::postponeCallback( void ) {
+  _postponeCallback = true;
+}
+
+void SonarConfiguration::enableCallback( void ) {
+  _postponeCallback = false;
+  sendCallback();
+}
+
+void SonarConfiguration::sendCallback( ) {
+  if( _callback && !_postponeCallback ) {
+     _callback( *this );
+  }
+}
+
+// need to integrate flags into dyanmic reconfig
+void SonarConfiguration::setFlags(uint8_t flags)
+{
+  _sfm.flags = flags;
+  sendCallback();
+}
+
+void SonarConfiguration::setRange(double input)
+{
+  // 40 meters is the max range for the 1200d model
+  // may need to use a double instead of uint8_t (depends on flags)
+  if (input <= 40 && input > 0) {
+    _sfm.range = input;
   }
 
-  void SonarConfiguration::postponeCallback( void ) {
-    _postponeCallback = true;
-  }
+  sendCallback();
+}
 
-  void SonarConfiguration::enableCallback( void ) {
-    _postponeCallback = false;
-    sendCallback();
+void SonarConfiguration::setGainPercent(double input)
+{
+  if (input <= 100 && input > 0) {
+    _sfm.gainPercent = input;
   }
+  sendCallback();
+}
 
-  void SonarConfiguration::sendCallback( ) {
-    if( _callback && !_postponeCallback ) {
-       _callback( *this );
-    }
+void SonarConfiguration::setGamma(double input)
+{
+  if (input <= 255 && input > 0) {
+    _sfm.gammaCorrection = input;
   }
+  sendCallback();
+}
 
-  // need to integrate flags into dyanmic reconfig
-  void SonarConfiguration::setFlags(uint8_t flags)
-  {
-    _sfm.flags = flags;
-    sendCallback();
-  }
+void SonarConfiguration::setPingRate(PingRateType newRate)
+{
+  _sfm.pingRate = newRate;
+  sendCallback();
+}
 
-  void SonarConfiguration::setRange(double input)
-  {
-    // 40 meters is the max range for the 1200d model
-    // may need to use a double instead of uint8_t (depends on flags)
-    if (input <= 40 && input > 0) {
-      _sfm.range = input;
-    }
+void SonarConfiguration::setFreqMode(OculusFreqMode input)
+{
+  _sfm.masterMode = input;
+  sendCallback();
+}
 
-    sendCallback();
-  }
-
-  void SonarConfiguration::setGainPercent(double input)
-  {
-    if (input <= 100 && input > 0) {
-      _sfm.gainPercent = input;
-    }
-    sendCallback();
-  }
-
-  void SonarConfiguration::setGamma(double input)
-  {
-    if (input <= 255 && input > 0) {
-      _sfm.gammaCorrection = input;
-    }
-    sendCallback();
-  }
-
-  void SonarConfiguration::setPingRate(PingRateType newRate)
-  {
-    _sfm.pingRate = newRate;
-    sendCallback();
-  }
-
-  void SonarConfiguration::setFreqMode(OculusFreqMode input)
-  {
-    _sfm.masterMode = input;
-    sendCallback();
-  }
-
-  void SonarConfiguration::serializeTo( boost::asio::streambuf &stream ) const
-  {
-    stream.sputn( reinterpret_cast<const char*>(&_sfm), sizeof(OculusSimpleFireMessage) );
-  }
+void SonarConfiguration::serializeTo( boost::asio::streambuf &stream ) const
+{
+  stream.sputn( reinterpret_cast<const char*>(&_sfm), sizeof(OculusSimpleFireMessage) );
+}
 
 }
