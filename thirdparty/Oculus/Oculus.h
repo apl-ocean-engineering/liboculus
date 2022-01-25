0c4ae54b28619c57c69bf48a99a13c54f45d39cb
Aaron Marburg <amarburg@uw.edu>
2022-01-25 14:16:32 -0800

    Initial copy of new Oculus header from Blueprint with OculusSimpleFireMessage2 and OculusSimplePingResult2

--- thirdparty/Oculus/Oculus.h
+++ thirdparty/Oculus/Oculus.h
@@ -96,27 +96,32 @@ enum DataSizeType : uint8_t
 // -----------------------------------------------------------------------------
 enum OculusPartNumberType : uint16_t
 {
-	partNumberUndefined			= 0,
+    partNumberUndefined = 0,
 
-	partNumberM370s				= 1041,
-	partNumberM370s_Artemis		= 1229,
-	partNumberM370s_Deep		= 1217,
-	partNumberM373s				= 1209,
-	partNumberM373s_Deep		= 1218,
+    partNumberM370s = 1041,
+    partNumberMT370s = 2418,
+    partNumberMD370s = 1433,
+    partNumberMD370s_Burton = 2294,
+    partNumberMD370s_Impulse = 1217,
 
-	partNumberM750d				= 1032,
-	partNumberM750d_Fusion		= 1134,
-	partNumberM750d_Artemis		= 1135,
+    partNumberM750d = 1032,
+    partNumberMT750d = 2419,
+    partNumberMD750d = 1434,
+    partNumberMD750d_Burton = 1921,
+    partNumberMD750d_Impulse = 1244,
 
-	partNumberM1200d			= 1042,
-	partNumberM1200d_Deep		= 1219,
-	partNumberM1200d_Artemis	= 1228,
+    partNumberM1200d = 1042,
+    partNumberMT1200d = 2420,
+    partNumberMD1200d = 1435,
+    partNumberMD1200d_Burton = 2086,
+    partNumberMD1200d_Impulse = 1219,
 
-	partNumberN1200s			= 1220,
-	partNumberN1200s_Deep		= 1221,
+    partNumberM3000d = 2203,
+    partNumberMT3000d = 2599,
+    partNumberMD3000d_Burton = 2659,
+    partNumberMD3000d_Impulse = 2658,
 
-
-	partNumberEnd				= 0xFFFF
+    partNumberEnd = 0xFFFF
 };
 struct OculusMessageHeader
 {
@@ -131,7 +136,7 @@ public:
 };
 
 // -----------------------------------------------------------------------------
-struct OculusSimpleFireMessage
+typedef struct 
 {
 public:
   OculusMessageHeader head;     // The standard message header
@@ -151,33 +156,74 @@ public:
   double gainPercent;           // The gain demand
   double speedOfSound;          // ms-1, if set to zero then internal calc will apply using salinity
   double salinity;              // ppt, set to zero if we are in fresh water
-};
+} OculusSimpleFireMessage;
+
+typedef struct {
+	OculusMessageHeader head;
+	uint8_t masterMode;
+	PingRateType pingRate;
+	uint8_t networkSpeed; /* The max network speed in Mbs , set to 0x00 or 0xff to use link speed */
+	uint8_t gammaCorrection; /* The gamma correction - 255 is equal to a gamma correction of 1.0 */
+	uint8_t flags;
+	double rangePercent; /* The range demand (%) */
+	double gainPercent; /* The percentage gain */
+	double speedOfSound; /* The speed of sound - set to zero to use internal calculations */
+	double salinity; /* THe salinity to be used with internal speed of sound calculations (ppt) */
+	uint32_t extFlags;
+	uint32_t reserved[8];
+} OculusSimpleFireMessage2;
 
 // -----------------------------------------------------------------------------
-struct OculusSimplePingResult
+typedef struct 
 {
 public:
-  OculusSimpleFireMessage fireMessage;
-  uint32_t pingId; 			/* An incrementing number */
+    OculusSimpleFireMessage fireMessage;
+    uint32_t pingId; 			/* An incrementing number */
+    uint32_t status;
+    double frequency;				/* The acoustic frequency (Hz) */
+    double temperature;				/* The external temperature (deg C) */
+    double pressure;				/* The external pressure (bar) */
+    double speeedOfSoundUsed;		/* The actual used speed of sound (m/s). May be different to the speed of sound set in the fire message */
+    uint32_t pingStartTime;
+    DataSizeType dataSize; 			/* The size of the individual data entries */
+    double rangeResolution;			/* The range in metres corresponding to a single range line */
+    uint16_t nRanges;			/* The number of range lines in the image*/
+    uint16_t nBeams;			/* The number of bearings in the image */
+    uint32_t imageOffset; 		/* The offset in bytes of the image data from the start of the network message */
+    uint32_t imageSize; 		/* The size in bytes of the image data */
+    uint32_t messageSize; 		/* The total size in bytes of the network message */
+    // *** NOT ADDITIONAL VARIABLES BEYOND THIS POINT ***
+    // There will be an array of bearings (shorts) found at the end of the message structure
+    // Allocated at run time
+    // short bearings[];
+    // The bearings to each of the beams in 0.01 degree resolution
+} OculusSimplePingResult;
+
+typedef struct {
+  OculusSimpleFireMessage2 fireMessage;
+  uint32_t pingId; 		/* An incrementing number */
   uint32_t status;
-  double frequency;				/* The acoustic frequency (Hz) */
-  double temperature;				/* The external temperature (deg C) */
-  double pressure;				/* The external pressure (bar) */
-  double speedOfSoundUsed;		/* The actual used speed of sound (m/s). May be different to the speed of sound set in the fire message */
-  uint32_t pingStartTime;
-  DataSizeType dataSize; 			/* The size of the individual data entries */
-  double rangeResolution;			/* The range in metres corresponding to a single range line */
-  uint16_t nRanges;			/* The number of range lines in the image*/
-  uint16_t nBeams;			/* The number of bearings in the image */
-  uint32_t imageOffset; 		/* The offset in bytes of the image data from the start of the network message */
+  double frequency;		/* The acoustic frequency (Hz) */
+  double temperature;		/* The external temperature (deg C) */
+  double pressure;			/* The external pressure (bar) */
+  double heading;			/* The heading (degrees) */
+  double pitch;			/* The pitch (degrees) */
+  double roll;			/* The roll (degrees) */
+  double speeedOfSoundUsed;	/* The actual used speed of sound (m/s) */
+  double pingStartTime;		/* In seconds from sonar powerup (to microsecond resolution) */
+  DataSizeType dataSize; 		/* The size of the individual data entries */
+  double rangeResolution;		/* The range in metres corresponding to a single range line */
+  uint16_t nRanges;		/* The number of range lines in the image*/
+  uint16_t nBeams;		/* The number of bearings in the image */
+  uint32_t spare0;
+  uint32_t spare1;
+  uint32_t spare2;
+  uint32_t spare3;
+  uint32_t imageOffset; 	/* The offset in bytes of the image data from the start */
   uint32_t imageSize; 		/* The size in bytes of the image data */
-  uint32_t messageSize; 		/* The total size in bytes of the network message */
-  // *** NOT ADDITIONAL VARIABLES BEYOND THIS POINT ***
-  // There will be an array of bearings (shorts) found at the end of the message structure
-  // Allocated at run time
-  // short bearings[];
-  // The bearings to each of the beams in 0.01 degree resolution
-};
+  uint32_t messageSize; 	/* The total size in bytes of the network message */
+  //uint16_t bearings[OSS_MAX_BEAMS]; /* The brgs of the formed beams in 0.01 degree resolution */
+} OculusSimplePingResult2;
 
 // -----------------------------------------------------------------------------
 struct OculusVersionInfo
@@ -201,7 +247,7 @@ public:
   OculusDeviceType   deviceType;
   OculusPartNumberType partNumber;
   uint32_t   status;
-  OculusVersionInfo versionInfo;
+  OculusVersionInfo versinInfo;
   uint32_t   ipAddr;
   uint32_t   ipMask;
   uint32_t   connectedIpAddr;
@@ -233,6 +279,235 @@ struct OculusUserConfigMessage {
 	OculusUserConfig config;
 };
 
+typedef struct
+{
+	uint8_t       b0;
+	double        d0;
+	double        range;
+	double        d2;
+	double        d3;
+	double        d4;
+	double        d5;
+	double        d6;
+	uint16_t      nBeams;
+	double        d7;
+	uint8_t		  b1;
+	uint8_t		  b2;
+	uint8_t		  b3;
+	uint8_t		  b4;
+	uint8_t		  b5;
+	uint8_t       b6;
+	uint16_t      u0;
+	uint8_t       b7;
+	uint8_t       b8;
+	uint8_t       b9;
+	uint8_t       b10;
+	uint8_t       b11;
+	uint8_t       b12;
+	uint8_t       b13;
+	uint8_t       b14;
+	uint8_t       b15;
+	uint8_t       b16;
+	uint16_t      u1;
+} PingConfig;
+
+typedef struct
+{
+  uint8_t b0;
+  double d0;
+  uint16_t u0;
+  uint16_t u1;
+} s0;
+
+
+typedef struct
+{
+  uint8_t b0;
+} s2;
+
+typedef struct
+{
+  uint8_t b0;
+  uint8_t b1;
+} s7;
+
+typedef struct
+{
+  int i0;
+  int i1;
+  int i2;
+  int i3;
+  int i4;
+  int i5;
+} s9;
+
+typedef struct
+{
+  uint8_t b0;
+  double d0;
+  double d1;
+} s8;
+
+typedef struct
+{
+  uint8_t  b0;
+  uint16_t u0;
+  uint8_t  b1;
+  double   d0;
+} s1;
+
+typedef struct
+{
+  uint8_t       b0;
+  uint8_t        b1;
+  uint8_t      b2;
+  uint8_t       b3;
+  uint8_t            b4;
+  uint8_t b5;
+  uint8_t      b6;
+  uint8_t   b7;
+  uint8_t        b8;
+  uint8_t      b9;
+  uint8_t       b10;
+  uint8_t            b11;
+  uint8_t    b12;
+  uint8_t            b13;
+  uint8_t            b14;
+  uint8_t            b15;
+  uint16_t           u0;
+  uint8_t    b16;
+  double             d0;
+  double             d1;
+} s3;
+
+typedef struct
+{
+  uint8_t  b0;
+  uint8_t  b1;
+  double   d0;
+  double   d1;
+  double   d2;
+} s4;
+
+typedef struct
+{
+
+  uint8_t  b0;
+  uint8_t  b1;
+  uint16_t u0;
+  uint16_t u1;
+  uint16_t u2;
+  uint16_t u3;
+  uint16_t u4;
+  uint16_t u5;
+} s5;
+
+typedef struct
+{
+  double d0;
+  double d1;
+} s6;
+
+typedef struct
+{
+  int i0;
+  int i1;
+  int i2;
+  int i3;
+} s10;
+
+typedef struct
+{
+  double d0;
+  double d1;
+  double d2;
+  double d3;
+  double d4;
+} s11;
+
+typedef struct
+{
+  double d0;
+  double d1;
+  double d2;
+  double d3;
+  double d4;
+  double d5;
+  double d6;
+} s12;
+
+typedef struct
+{
+	uint32_t u0;
+	uint32_t   u1;
+	double d1;
+	double d2;
+	uint32_t   u2;
+	uint32_t   u3;
+
+
+	double d3;
+	double d4;
+	double d5;
+	double d6;
+	double d7;
+	double d8;
+	double d9;
+	double d10;
+	double d11;
+	double d12;
+	double d13;
+	double d14;
+	double d15;
+	double d16;
+	double d17;
+	double d18;
+	double d19;
+	double d20;
+
+	uint32_t   u4;
+	uint32_t   nRangeLinesBfm;
+	uint16_t u5;
+	uint16_t u6;
+	uint16_t u7;
+	uint32_t   u8;
+	uint32_t   u9;
+	uint8_t  b0;
+	uint8_t  b1;
+	uint8_t  b2;
+	uint32_t   imageOffset;              // The offset in bytes of the image data (CHN, CQI, BQI or BMG) from the start of the buffer
+	uint32_t   imageSize;                // The size in bytes of the image data (CHN, CQI, BQI or BMG)
+	uint32_t   messageSize;              // The total size in bytes of the network message
+	// *** NOT ADDITIONAL VARIABLES BEYOND THIS POINT ***
+	// There will be an array of bearings (shorts) found at the end of the message structure
+	// Allocated at run time
+	// short bearings[];
+	// The bearings to each of the beams in 0.01 degree resolution
+} PingParameters;
+
+typedef struct
+{
+	OculusMessageHeader head;
+	PingConfig          ping;
+	s0					t0;
+	s1					t1;
+	s2          t2;
+	s3       t3;
+	s4         t4;
+	s5          t5;
+	s6        t6;
+	s7         t7;
+	s8   t8;
+	s9        t9;
+	s10         t10;
+	s11           t11;
+	s12          t12;
+	PingParameters      ping_params;
+} OculusReturnFireMessage;
+
+
+#include <QString>
+
 // Oculus configuration information
 struct OculusInfo {
 	// Part number for which the info relates to
@@ -246,7 +521,7 @@ struct OculusInfo {
 	// Maximum high-frequency range
 	double maxHF;
 	// Description
-	const char* model;
+	char* model;
 };
 
 #pragma pack(pop)
@@ -255,107 +530,58 @@ struct OculusInfo {
 // If the part number is not in this list, it defaults to the "partNumberUndefined"
 // configuration
 const OculusInfo OculusSonarInfo[] = {
-	// All undefined sonars and sonars not in this list
-	{
-		partNumberUndefined,
-		// Supports LF mode
-		true,
-		// Up to 120m range
-		120,
-		// Supports HF mode
-		true,
-		// Up to 40m range
-		40,
-    "partNumberUndefined"
-	},
-	// -------------------------------------------------------------------------
-	// M370
-	{
-		partNumberM370s,
-		// Supports LF mode
-		true,
-		// Up to 200m range
-		200,
-		// No HF mode
-		false,
-		// Up to 40m range
-		-1,
-    "M370s"
-	},
-	// M370s
-	{
-		partNumberM370s_Artemis,
-		// Supports LF mode
-		true,
-		// Up to 200m range
-		200,
-		// No HF mode
-		false,
-		-1,
-    "M370s_Artemis"
-	},
-	// M370s_Deep
-	{
-		partNumberM370s_Deep,
-		// Supports LF mode
-		true,
-		// Up to 200m range
-		200,
-		// No HF mode
-		false,
-		-1,
-    "M370s_Deep"
-	},
-	// M373
-	{
-		partNumberM373s,
-		// Supports LF mode
-		true,
-		// Up to 200m range
-		200,
-		// No HF mode
-		false,
-		-1,
-    "M373s"
-	},
-	// M373s_Deep
-	{
-		partNumberM373s_Deep,
-		// Supports LF mode
-		true,
-		// Up to 200m range
-		200,
-		// No HF mode
-		false,
-		-1,
-    "M373s_Deep"
-	},
-	// -------------------------------------------------------------------------
-	// M1200d
-	{
-		partNumberM1200d,
-		// Supports LF mode
-		true,
-		// Up to 40m range
-		40,
-		// Supports HF mode
-		true,
-		// Up to 10m range
-		10,
-    "M1200d"
-	},
-	{
-		partNumberM1200d_Deep,
-		// Supports LF mode
-		true,
-		// Up to 40m range
-		40,
-		// Supports HF mode
-		true,
-		// Up to 10m range
-		10,
-    "M1200d_Deep"
-	},
-	// End of the list
-	{ partNumberEnd, false, -1, false, -1, "end" }
-};
+    // All undefined sonars and sonars not in this list
+    {
+        partNumberUndefined,
+        // Supports LF mode
+        true,
+        // Up to 120m range
+        120,
+        // Supports HF mode
+        true,
+        // Up to 40m range
+        40,
+    },
+    // -------------------------------------------------------------------------
+    // M370
+    {partNumberM370s,
+     // Supports LF mode
+     true,
+     // Up to 200m range
+     200,
+     // No HF mode
+     false,
+     // Up to 40m range
+     -1},
+
+    // M370s_Deep
+    {partNumberMD370s,
+     // Supports LF mode
+     true,
+     // Up to 200m range
+     200,
+     // No HF mode
+     false,
+     -1},
+    // -------------------------------------------------------------------------
+    // M1200d
+    {partNumberM1200d,
+     // Supports LF mode
+     true,
+     // Up to 40m range
+     40,
+     // Supports HF mode
+     true,
+     // Up to 10m range
+     10},
+    {partNumberMD1200d,
+     // Supports LF mode
+     true,
+     // Up to 40m range
+     40,
+     // Supports HF mode
+     true,
+     // Up to 10m range
+     10},
+    // End of the list
+    {partNumberEnd}};
