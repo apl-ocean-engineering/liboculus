29ca2822d780d71a1abd39e9503ca75c59fb150c
Aaron Marburg <amarburg@uw.edu>
2026-08-05 11:24:16 -0700

    Make system an optional component

--- cmake/BuildROS2.cmake
+++ cmake/BuildROS2.cmake
@@ -2,10 +2,12 @@
 
 find_package(ament_cmake REQUIRED)
 find_package(spdlog REQUIRED)
-find_package(Boost REQUIRED COMPONENTS system)
+find_package(Boost OPTIONAL_COMPONENTS system)
 
 add_library(oculus SHARED ${oculus_SRCS})
-target_link_libraries(oculus PUBLIC Boost::system spdlog::spdlog)
+target_link_libraries(oculus PUBLIC
+     Boost::system
+     spdlog::spdlog)
 
 target_include_directories(
     oculus
